24f6c8a26168d58f84188ae3068584d87167e4e7
lily <ldinkins@live.unc.edu>
2016-11-28 21:22:08 +0000

    Changed Presentation Script

--- bitcoins.md
+++ bitcoins.md
@@ -2,4 +2,3 @@
 % LILY 
 % NOVEMBER 2016
 
-pandoc -t revealjs --template=template-index.html -s --variable theme="black" --variable transition="slide" --variable revealjs-url="./" bitcoins.md -o index.html
--- build-presentation.sh
+++ build-presentation.sh
@@ -5,4 +5,4 @@ TEMPLATE=template-index.html
 TRANSITION=slide
 THEME=black
 
-pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" $INPUT.md -o index.html
+pandoc -t revealjs --template=template-index.html -s --variable theme="black" --variable transition="slide" --variable revealjs-url="./" bitcoins.md -o index.html
